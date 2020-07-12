cc9eb74d1e97a008e62ac9f934e43ae9afe11997
鑫龙魂 <liyaqing_it@126.com>
2020-07-12 21:05:40 +0800

    1.测试滚动列表点赞

--- my_flutter/lib/main.dart
+++ my_flutter/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:english_words/english_words.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -12,7 +12,15 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      // home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new Scaffold(
+        // appBar: new AppBar(
+        //   title: new Text('dd')
+        // ),
+        body: new Center(
+          child: new RandomWords(),
+        )
+      ),
     );
   }
 }
@@ -291,4 +299,100 @@ class _MyHomePageState extends State<MyHomePage> {
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return new RandomWordState();
+  }
+}
+
+class RandomWordState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+  final _saved = new Set<WordPair>();
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('title'),
+        actions: <Widget> [
+          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved,)
+        ]
+      ),
+      body: _buildSuggestions(),
+    );
+    // return _buildSuggestions();
+  }
+
+  void _pushSaved () {
+    Navigator.of(context).push(
+      new MaterialPageRoute(
+        builder: (context) {
+          final titles = _saved.map((pair){
+            return new ListTile(
+              title: new Text(
+                pair.asPascalCase,
+                style: _biggerFont,
+              ),
+            );
+          });
+          final divided = ListTile.divideTiles(
+            context: context,
+            tiles: titles,
+          ).toList();
+          return new Scaffold(
+            appBar: new AppBar(
+              title: new Text('saved'),
+            ),
+            body: new ListView(children:divided),
+          );
+        }
+      
+      )
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
+    return new ListTile(
+      title: new Text(
+        pair.asPascalCase,
+        style: _biggerFont
+      ),
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) {
+          return new Divider();
+        }
+        final index = i ~/2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+  
 }
